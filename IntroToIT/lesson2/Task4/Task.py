e9c83440839cbb3fc65bc175f051689e5b07651c
Kartofel11 <huilovpisa@gmail.com>
2023-10-13 21:16:18 +0300

    fix

--- IntroToIT/lesson2/Task4/Task.py
+++ IntroToIT/lesson2/Task4/Task.py
@@ -1,7 +1,7 @@
 #INTRO TO IT 2nd COURSE
 # Задача 4: Найди сумму!
 # Можешь найти сумму всех чисел в списке?
-определенная сумма чисел(numbers):
- возвращаемая сумма(числа)
-сумма = [1, 2, 3, 4, 5]
-print(f"Сумма {чисел} равна {из чисел(numbers)}")
+a certain amount of numbers(numbers):
+the amount to be returned (numbers)
+is the amount = [1, 2, 3, 4, 5]
+print(f"The sum of {numbers} is equal to {of numbers(numbers)}")
